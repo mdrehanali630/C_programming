8eb21aef032b1d12a8446f0688e617116dd33f22
mdrehanali630 <mdrehanali630@gmail.com>
2025-11-26 09:27:42 +0530

    Add files via upload

---
+++ _.c sum two variable user.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+
+void main()
+{
+    int a;
+    int b;
+    int sum;
+    printf("Enter the first number:\n");
+    scanf("%d", &a);
+    printf("Enter the second number:\n");
+    scanf("%d", &b);
+    sum=a+b;
+    printf("The sum of %d and %d is %d",a,b,sum);
+    }
+    
