1f76f397dadcc549063ea2339d9a4d3db1e45a80
mdrehanali630 <mdrehanali630@gmail.com>
2026-02-06 09:44:45 +0530

    Create Login to instagram if the password is correct.C

---
+++ Login to instagram if the password is correct.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+  void main()
+  {
+   printf("welcome to instagram \n");
+   int password=4593;
+   int upwd;
+   printf("Enter the password \n");
+   scanf("%d",&upwd);
+   if (password==upwd)
+   {
+    printf("Name:Magician \n");
+    printf("Age:50 \n");
+    }
+     printf("Program ends");
+     }
