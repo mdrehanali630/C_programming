dccefdddeff5ff49189cc645586b55d9c4ee2280
mdrehanali630 <mdrehanali630@gmail.com>
2026-02-07 09:29:32 +0530

    Create Check the given number is even or odd by using if else condition.C

---
+++ Check the given number is even or odd by using if else condition.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ void main()
+  {
+   int a;
+   printf("Program starts \n");
+   printf("Enter the value of a \n");
+   scanf("%d",&a);
+   if(a%2!=0)
+   { 
+    printf("%d is odd \n",a);
+    }
+    else
+    {
+     printf("%d is even \n",a);
+     }
+     printf("Program ends");
+     }
