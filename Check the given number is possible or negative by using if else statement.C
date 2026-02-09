5c5bd35ac95f13f692e9e9913306bfb33be020ce
mdrehanali630 <mdrehanali630@gmail.com>
2026-02-09 08:42:35 +0530

    Create Check the given number is possible or negative by using if else statement.C

---
+++ Check the given number is possible or negative by using if else statement.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ void main()
+  {
+   int a;
+   printf("Program starts \n");
+   printf("Enter the value of a \n");
+   scanf("%d",&a);
+   if(a>0)
+   { 
+    printf("%d is positive value\n",a);
+    }
+    else
+    {
+     printf("%d is negative value \n",a);
+     }
+     printf("Program ends");
+     }
