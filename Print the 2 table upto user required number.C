6c553e6058ccb6f58b6d4b550c6d8132a3249830
mdrehanali630 <mdrehanali630@gmail.com>
2026-04-06 16:49:15 +0530

    Create Print the 2 table upto user required number.C

---
+++ Print the 2 table upto user required number.C
@@ -0,0 +1,11 @@
+       #include <stdio.h>
+       void main()
+       {
+         int n;
+         printf("upto hom many values are required\n");
+         scanf("%d",&n);
+         for(int a=1;a<=n;a++)
+         {
+          printf("2*%d=%d\n",a,2*a);
+         }
+       }   
