451a341af936e10e09ee16a710f4c74e81766863
mdrehanali630 <mdrehanali630@gmail.com>
2026-04-15 17:27:54 +0530

    Create Print the 2 table in reverse upto user required.C

---
+++ Print the 2 table in reverse.C
@@ -0,0 +1,12 @@
+       #include <stdio.h>
+       void main()
+       {
+         int n;
+         printf("upto hom many values are required\n");
+         scanf("%d",&n);
+         for(int a=9;a>=n;a--)
+         {
+          printf("2*%d=%d\n",a,2*a);
+         }
+       }   
+       
