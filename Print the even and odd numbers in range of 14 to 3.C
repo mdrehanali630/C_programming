2d155953ee3c889ebbc7db00f75385539784e7be
mdrehanali630 <mdrehanali630@gmail.com>
2026-04-16 17:00:04 +0530

    Create Print the even and odd numbers in range of 14 to 3.C

---
+++ Print the even and odd numbers in range of 14 to 3.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  void main()
+  {
+   for(int a=14;a>=3;a--)
+    { 
+     if(a%2==0)
+      {
+       printf("a=%d is even \n",a);
+      }
+      else
+      {
+      printf("a=%d is odd \n",a);
+      }
+    }
+   }   
+ 
+    
