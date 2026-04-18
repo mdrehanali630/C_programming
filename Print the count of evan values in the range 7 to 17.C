2980ff806a0f510b18908e814739775106cd7b09
mdrehanali630 <mdrehanali630@gmail.com>
2026-04-18 09:03:24 +0530

    Create Print the count of evan values in the range 7 to 17.C

---
+++ Print the count of evan values in the range 7 to 17.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ void main()
+ {
+  int count=0;
+  for(int a=7;a<=17;a++)
+  {
+   if(a%2==0)
+   {
+    count++;
+    }
+   }
+   printf("There are %d even numbers in 7 to 17",count);  
+ }   
+    
