7f30e66bd84879b4ff25fea1eaac5f137f753557
mdrehanali630 <mdrehanali630@gmail.com>
2026-05-01 08:35:28 +0530

    Create Check the given number is even or odd by using user define function.C

---
+++ Check the given number is even or odd by using user define function.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+ void even_odd();
+ void main()
+ {
+  int a;
+  printf("Enter  the value of a\n");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+   printf("%d is even number \n",a);
+   }
+   else
+   {
+    printf("%d is odd number\n",a);
+    }
+   }
+    void even_odd()
+  {
+    printf("Even_odd.star \n");
+    int a;
+    printf("#nter the number\n");
+    scanf("%d",a);
+    if(a%2==0)
+    {
+     printf("%d is even number\n",a);
+    }
+    else
+    {
+    printf("%d is odd number\n",a);
+    }
+  }
+   
